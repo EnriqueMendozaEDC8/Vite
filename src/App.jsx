7c37a64a642e10b846ef688c3ebda11dd093c139
Enrique Mendoza <enrique.510.06@gmail.com>
2022-09-16 05:02:50 +0000

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -19,23 +19,7 @@ function App() {
           Edit <code>App.jsx</code> and save to test HMR updates.
         </p>
         <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
+          Hola mi nombre es Enrique Mendoza
         </p>
       </header>
     </div>
